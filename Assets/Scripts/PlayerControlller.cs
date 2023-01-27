09d36b5b5064752570430961b7c40e468ecab14e
yyandas <yigityandas@gmail.com>
2023-01-27 22:46:15 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerControlller.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlller : MonoBehaviour
+{
+    public float moveSpeed;
+    public CharacterController charCon;
+
+    private Vector3 moveInput;
+
+    public Transform camTrans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        charCon.Move(moveInput);
+
+        //camera movement
+
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x ,transform.rotation.eulerAngles.z);
+
+        camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3 (-mouseInput.y,0f,0f));
+
+    }
+}
